79536eb1f50771b4096046543d3f6f4953f53bd5
Nadia Shabbir <nadia.shabbir4343@gmail.com>
2026-04-18 23:36:04 +0500

    Add RootLayout component with ThemeProvider

---
+++ Amazon-FAQ-Customer-Chatbot/frontend/app/layout.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { ThemeProvider } from "next-themes"
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange={false}
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
